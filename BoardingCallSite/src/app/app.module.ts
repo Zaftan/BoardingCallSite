467a07002ff31e65be3b73e93dda2381db1c1d86
BRO <Brynn.rothuis@hotmail.com>
2020-09-03 12:44:28 +0200

    Merge conflicts resolved

--- BoardingCallSite/src/app/app.module.ts
+++ BoardingCallSite/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
