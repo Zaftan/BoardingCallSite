15ad8405b9bfb6630d4399947c22c7ee39b63fd2
BRO <Brynn.rothuis@hotmail.com>
2020-09-03 14:18:52 +0200

    Routing and Landing Page beginnings
    Coauthored with @wmeermans

--- BoardingCallSite/src/app/app-routing.module.ts
+++ BoardingCallSite/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { LandingComponent } from './landing/landing.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
   { path: 'aboutUs', component: AboutUsComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: '**', component: LandingComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
