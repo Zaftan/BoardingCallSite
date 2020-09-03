a8806539ff905a15af47169b82ad67423248210a
BRO <Brynn.rothuis@hotmail.com>
2020-09-03 12:47:43 +0200

    pull push hotfix
    ~Hotfix gitkraken

--- BoardingCallSite/src/app/app-routing.module.ts
+++ BoardingCallSite/src/app/app-routing.module.ts
@@ -4,15 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
-<<<<<<< Updated upstream
   { path: 'aboutUs', component: AboutUsComponent },
 ];
-=======
-  {path: 'landing', component: LandingComponent},
-  { path: '**', component: LandingComponent }
-];
-
->>>>>>> Stashed changes
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
