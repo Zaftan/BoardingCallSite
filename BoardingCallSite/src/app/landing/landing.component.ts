363adc75da62c4527ca13c73c94110a7b552a98b
BRO <Brynn.rothuis@hotmail.com>
2020-09-04 15:20:11 +0200

    carousel start

--- BoardingCallSite/src/app/landing/landing.component.ts
+++ BoardingCallSite/src/app/landing/landing.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class LandingComponent implements OnInit {
+  slides: string [] = ['/src/assets/placeholder.jpg', '/src/assets/Pictures/839px-Ubisoft_logo.png', './assets/Knot_Slide_3.jpg'];
+  info: string [] = ['uno', 'dos', 'tre', 'quatre'];
+  i = 0;
+  maxSlides = 3;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  getSlide(): string
+  {
+    return this.slides[this.i];
   }
+
+  getPrev(): void
+  {
+    this.i = this.i === 0 ? 0 : this.i - 1;
+  }
+
+  getNext(): void
+  {
+    this.i = this.i === this.slides.length ? this.i : this.i + 1;
+  }
+
+  slideInfo(): string
+  {
+    return this.info[this.i];
+  }
+
 }
