a55c82bcd87b21492ab028427082bf4c2f9cb406
Wouter Meermans <wouter.meermans@student.graafschapcollege.nl>
2020-09-10 16:35:40 +0200

    Rules Page

--- BoardingCallSite/src/app/app-routing.module.ts
+++ BoardingCallSite/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { DamageCalcComponent } from './damage-calc/damage-calc.component';
+import {RulesComponent} from './rules/rules.component';
 
 const routes: Routes = [
   { path: 'aboutUs', component: AboutUsComponent },
   { path: 'Calculator', component: DamageCalcComponent },
   { path: 'landing', component: LandingComponent },
+  { path: 'Rules', component: RulesComponent },
   { path: '**', component: LandingComponent },
 ];
 @NgModule({
