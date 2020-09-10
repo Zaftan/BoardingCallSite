ac0e34a0152ce00d3aa02fe7de2dc4ad125ebe44
Wouter Meermans <wouter.meermans@student.graafschapcollege.nl>
2020-09-10 21:26:55 +0200

    Damage calc fixed

--- BoardingCallSite/src/app/damage-calc/damage-calc.component.ts
+++ BoardingCallSite/src/app/damage-calc/damage-calc.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, NgForm, FormGroup } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators, NgForm, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-damage-calc',
@@ -8,7 +8,6 @@ import { FormBuilder, Validators, NgForm, FormGroup } from '@angular/forms';
 })
 export class DamageCalcComponent implements OnInit {
   weatherConditions: string [] = ['Sunny', 'Rainy', 'Stormy', 'Foggy'];
-  weatherChange: 25;
   turn = 0;
   weather;
   weathername;
@@ -19,14 +18,14 @@ export class DamageCalcComponent implements OnInit {
   weapons;
   form: FormGroup;
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.turnButton();
   }
 
-  turnButton(): void
-  {
+  turnButton(): void {
     this.turn++;
     this.forecast = Math.floor(Math.random() * Math.floor(4));
     this.forecastImg(this.forecast);
@@ -34,138 +33,77 @@ export class DamageCalcComponent implements OnInit {
   }
 
   forecastImg(forecast): void {
-    if (forecast === 0)
-    {
+    if (forecast === 0) {
       this.weather = '../../assets/Pictures/Sunny.png';
       return this.weather;
-    }
-    else if (forecast === 1)
-    {
+    } else if (forecast === 1) {
       this.weather = '../../assets/Pictures/Rainy.png';
       return this.weather;
-    }
-    else if (forecast === 2)
-    {
+    } else if (forecast === 2) {
       this.weather = '../../assets/Pictures/Stormy.png';
       return this.weather;
-    }
-    else
-    {
+    } else {
       this.weather = '../../assets/Pictures/Foggy.png';
       return this.weather;
     }
   }
 
   weathernames(forecast): void {
-    if (forecast === 0)
-    {
+    if (forecast === 0) {
       this.weathername = this.weatherConditions[forecast];
       return this.weathername;
-    }
-    else if (forecast === 1)
-    {
+    } else if (forecast === 1) {
       this.weathername = this.weatherConditions[forecast];
       return this.weathername;
-    }
-    else if (forecast === 2)
-    {
+    } else if (forecast === 2) {
       this.weathername = this.weatherConditions[forecast];
       return this.weathername;
-    }
-    else
-    {
+    } else {
       this.weathername = this.weatherConditions[forecast];
       return this.weathername;
     }
   }
 
-  damageCalc(data)
-  {
+  damageCalc(data) {
     this.basedamage = 0;
     this.weapons = data.weaponbugs;
     this.distance = data.distance;
-    let weatherboost = this.damage(this.forecast);
-    let gunboost = this.weapon(this.weapons, this.distance);
-    return this.result = weatherboost + gunboost;
+    return this.result =this.weapon(this.weapons, this.distance);
   }
 
-  damage(forecast)
-  {
-    if (forecast === 0)
-    {
-      //Sunny
-      return this.basedamage += 0;
+  weapon(weapons, distance) {
+    let miss = Math.floor(Math.random() * 100 + 1);
+    if (miss <= 5) {
+      return this.basedamage = 0;
     }
-    else if (forecast === 1)
-    {
-      //Rainy
-      return this.basedamage += 0;
-    }
-    else if (forecast === 2)
-    {
-      //Stormy
-      return this.basedamage += 0;
-    }
-    else
-    {
-      //Foggy
-      return this.basedamage += 0;
-    }
-  }
-
-  weapon(weapons, distance)
-  {
-    if (this.forecast === 3)
-    {
-      let miss = Math.floor(Math.random() * 100 + 1);
-      if (miss === 1 || 2 || 3)
-      {
+    if (this.forecast === 3) {
+      if (miss <= 20) {
         return this.basedamage = 0;
       }
-      else
-      {
-        if (this.forecast === 2)
-        {
-          let luckyshot = Math.floor(Math.random() * 100 + 1);
-          if (luckyshot === 1)
-          {
-            this.basedamage += 1;
-          }
-        }
-        else
-        {
-          this.basedamage + 0;
-        }
-        if (weapons === 1 && distance >= 1 || distance <= 3)
-        {
-          //Shotgun
-          return this.basedamage += 5;
-        }
-        else if (weapons === 2 && distance >= 3 || distance <= 6)
-        {
-          //Handgun
-          return this.basedamage += 2;
-        }
-        else if (weapons === 3 && distance >= 2 || distance <= 6)
-        {
-          //Bow
-          return this.basedamage += 3;
-        }
-        else if (weapons === 4 && distance >= 4 || distance <= 7)
-        {
-          //Sniper
-          return this.basedamage += 4;
-        }
-        else if (weapons === 5 && distance === 8)
-        {
-          //Katasect
-          return this.basedamage += 3;
-        }
-        else
-        {
-          return this.basedamage;
-        }
+    }
+
+    if (weapons == 1 && distance >= 1 && distance <= 3) {
+      // Bucksect
+      this.basedamage = 5;
+    } else if (weapons == 2 && distance >= 1 && distance <= 4) {
+      // Handgun
+      this.basedamage = 2;
+    } else if (weapons == 3 && distance >= 1 && distance <= 6) {
+      // Bowsect
+      this.basedamage = 3;
+    } else if (weapons == 4 && distance >= 4 && distance <= 7) {
+      // Sharpsect
+      this.basedamage = 4;
+    } else if (weapons == 5 && distance == 8) {
+      // Katasect
+     this.basedamage = 3;
+    }
+    if (this.forecast === 2) {
+      let luckyshot = Math.floor(Math.random() * 100 + 1);
+      if (luckyshot <= 3) {
+        this.basedamage += 1;
       }
     }
+    return this.basedamage;
   }
 }
